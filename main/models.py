074ff506b16a31c9b834e69d4d9ad70bbd392a82
Janos Gyerik <janos@axiom>
2011-08-07 19:41:21 +0200

    implemented vote model with unit tests

--- main/models.py
+++ main/models.py
@@ -23,6 +23,7 @@ class Hacker(DjangoUser):
     # Use UserManager to get the create_user method, etc.
     objects = UserManager()
 
+
 class OneLiner(models.Model):
     hacker = models.ForeignKey(Hacker)
 
@@ -38,8 +39,59 @@ class OneLiner(models.Model):
     def lines(self):
 	return [x for x in self.line.split('\n') if x.strip() != '']
 
+    def vote_up(self, hacker):
+	Vote.vote_up(hacker, self)
+
+    def vote_down(self, hacker):
+	Vote.vote_down(hacker, self)
+
+    def get_votes(self):
+	return (self.get_votes_up(), self.get_votes_down())
+
+    def get_votes_up(self):
+	return self.vote_set.filter(up=True).count()
+
+    def get_votes_down(self):
+	return self.vote_set.filter(up=False).count()
+
     def __unicode__(self):
-	return self.line
+	return self.summary
+
+
+class Vote(models.Model):
+    hacker = models.ForeignKey(Hacker)
+    oneliner = models.ForeignKey(OneLiner)
+    up = models.BooleanField(default=True)
+
+    created_dt = models.DateTimeField(default=datetime.now)
+
+    @staticmethod
+    def vote(hacker, oneliner, updown):
+	if oneliner.hacker == hacker:
+	    return
+
+	try:
+	    oneliner.vote_set.get(hacker=hacker, up=updown)
+	    return
+	except:
+	    pass
+
+	oneliner.vote_set.filter(hacker=hacker).delete()
+	Vote(hacker=hacker, oneliner=oneliner, up=updown).save()
+
+    @staticmethod
+    def vote_up(hacker, oneliner):
+	Vote.vote(hacker, oneliner, True)
+
+    @staticmethod
+    def vote_down(hacker, oneliner):
+	Vote.vote(hacker, oneliner, False)
+
+    def __unicode__(self):
+	return '%s %s %s' % (self.hacker.full_name, ('--', '++')[self.up], self.oneliner.summary)
+
+    class Meta:
+	unique_together = (('hacker', 'oneliner',),)
 
 
 # eof
--- main/tests.py
+++ main/tests.py
@@ -1,23 +1,61 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from main.models import Hacker, OneLiner, Vote
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+class Util:
+    @staticmethod
+    def new_hacker(username):
+	hacker = Hacker(username=username)
+	hacker.save()
+	return hacker
 
->>> 1 + 1 == 2
-True
-"""}
+    @staticmethod
+    def new_oneliner(hacker, line):
+	oneliner = OneLiner(hacker=hacker, line=line)
+	oneliner.save()
+	return oneliner
 
+    @staticmethod
+    def new_vote():
+	pass
+
+
+class VoteTests(TestCase):
+    def setUp(self):
+	self.jack = Util.new_hacker('jack')
+	self.jacks_oneliner = Util.new_oneliner(self.jack, 'echo jack')
+
+	self.mike = Util.new_hacker('mike')
+	self.mikes_oneliner = Util.new_oneliner(self.mike, 'echo mike')
+
+	self.frank = Util.new_hacker('frank')
+	self.franks_oneliner = Util.new_oneliner(self.frank, 'echo frank')
+
+    def test_multiple_vote_tolerance(self):
+	self.assertEquals(self.jacks_oneliner.get_votes_up(), 0)
+	self.jacks_oneliner.vote_up(self.jack)
+	self.assertEquals(self.jacks_oneliner.get_votes_up(), 0)
+	self.jacks_oneliner.vote_up(self.mike)
+	self.assertEquals(self.jacks_oneliner.get_votes_up(), 1)
+	self.jacks_oneliner.vote_up(self.mike)
+	self.assertEquals(self.jacks_oneliner.get_votes_up(), 1)
+	self.jacks_oneliner.vote_up(self.mike)
+	self.assertEquals(self.jacks_oneliner.get_votes_up(), 1)
+
+    def test_vote_counts(self):
+	self.assertEquals(self.jacks_oneliner.get_votes_up(), 0)
+	self.jacks_oneliner.vote_up(self.jack)
+	self.assertEquals(self.jacks_oneliner.get_votes_up(), 0)
+	self.jacks_oneliner.vote_up(self.mike)
+	self.assertEquals(self.jacks_oneliner.get_votes_up(), 1)
+	self.jacks_oneliner.vote_up(self.frank)
+	self.assertEquals(self.jacks_oneliner.get_votes_up(), 2)
+	self.jacks_oneliner.vote_up(self.frank)
+	self.assertEquals(self.jacks_oneliner.get_votes_up(), 2)
+	self.jacks_oneliner.vote_down(self.frank)
+	self.assertEquals(self.jacks_oneliner.get_votes_up(), 1)
+	self.assertEquals(self.jacks_oneliner.get_votes_down(), 1)
+	self.assertEquals(self.jacks_oneliner.get_votes(), (1, 1))
+
+
+# eof

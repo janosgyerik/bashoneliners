bf4fd30e7e9994e02408994951290e7d96e0c1bc
Janos Gyerik <janos@axiom>
2011-09-18 10:37:08 +0200

    skip social buttons when called from w3c validator

--- main/views.py
+++ main/views.py
@@ -20,6 +20,8 @@ def get_common_params(request):
     params = {
 	    'user': request.user,
 	    }
+    if request.META['HTTP_USER_AGENT'].startswith('W3C_VALIDATOR'):
+	params['w3c'] = True
     return params
 
 

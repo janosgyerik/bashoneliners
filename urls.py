cf984699483138eb96cf9abee58e25851240435c
Janos Gyerik <janos@axiom>
2011-10-08 09:45:39 +0200

    minor change to make it work on hosting provider

--- urls.py
+++ urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
-from main.models import LatestEntries
+from bashoneliners.main.models import LatestEntries
 
 urlpatterns = patterns('',
     # Example:

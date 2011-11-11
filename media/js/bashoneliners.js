694016c9ad4c96c9350df4df702035412d6dc343
Janos Gyerik <janos@axiom>
2011-11-11 06:51:23 +0100

    added expand all / collapse all

--- media/js/bashoneliners.js
+++ media/js/bashoneliners.js
@@ -12,6 +12,19 @@ function bind_details_trigger(obj) {
     $('<div class="details-trigger">Details...</div>').insertBefore(obj.find('.details')).click(function() {;
 	$(this).toggleClass('details-trigger-active').next().toggle('slow');
     });
+
+    obj.find('.expand-all').prepend('<div class="expand-all-trigger">Expand all</div>');
+    obj.find('.expand-all').prepend('<div class="collapse-all-trigger">Collapse all</div>');
+
+    obj.find('.expand-all-trigger').click(function() {;
+	obj.find('.details-trigger').addClass('details-trigger-active');
+	obj.find('.details').show('slow');
+    });
+
+    obj.find('.collapse-all-trigger').click(function() {;
+	obj.find('.details-trigger').removeClass('details-trigger-active');
+	obj.find('.details').hide('slow');
+    });
 }
 
 $(document).ready(function() {

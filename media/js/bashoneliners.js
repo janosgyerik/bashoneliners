b7ea61f135206eb87eac06d25aac09aa2a3e25da
Janos Gyerik <janos@axiom>
2011-12-23 22:10:19 +0100

    fixed a bug in the single one-liner view

--- media/js/bashoneliners.js
+++ media/js/bashoneliners.js
@@ -9,38 +9,41 @@
  */
 
 function bind_details_trigger(obj) {
-    obj.find('.details').each(function() {
-	var trigger = $('<div class="details-trigger"/>').click(function() {
-	    $(this).toggleClass('details-trigger-active').next().toggle('slow');
+    if (obj.find('.oneliner').size() > 3) {
+	obj.find('.details').each(function() {
+	    var trigger = $('<div class="details-trigger"/>').click(function() {
+		$(this).toggleClass('details-trigger-active').next().toggle('slow');
+	    });
+	    var has_explanation = $(this).find('.explanation').size() > 0;
+	    var has_limitations = $(this).find('.limitations').size() > 0;
+	    if (has_explanation && has_limitations) {
+		trigger.text('Show explanation and limitations');
+	    }
+	    else if (has_explanation) {
+		trigger.text('Show explanation');
+	    }
+	    else if (has_limitations) {
+		trigger.text('Show limitations');
+	    }
+	    $(this).before(trigger);
 	});
-	var has_explanation = $(this).find('.explanation').size() > 0;
-	var has_limitations = $(this).find('.limitations').size() > 0;
-	if (has_explanation && has_limitations) {
-	    trigger.text('Show explanation and limitations');
-	}
-	else if (has_explanation) {
-	    trigger.text('Show explanation');
-	}
-	else if (has_limitations) {
-	    trigger.text('Show limitations');
-	}
-	$(this).before(trigger);
-    });
 
-    if (obj.find('.oneliner').size() > 0) {
 	obj.find('.expand-all').prepend('<div class="expand-all-trigger">Expand all</div>');
 	obj.find('.expand-all').prepend('<div class="collapse-all-trigger">Collapse all</div>');
+
+	obj.find('.expand-all-trigger').click(function() {;
+	    obj.find('.details-trigger').addClass('details-trigger-active');
+	    obj.find('.details').show('slow');
+	});
+
+	obj.find('.collapse-all-trigger').click(function() {;
+	    obj.find('.details-trigger').removeClass('details-trigger-active');
+	    obj.find('.details').hide('slow');
+	});
     }
-
-    obj.find('.expand-all-trigger').click(function() {;
-	obj.find('.details-trigger').addClass('details-trigger-active');
-	obj.find('.details').show('slow');
-    });
-
-    obj.find('.collapse-all-trigger').click(function() {;
-	obj.find('.details-trigger').removeClass('details-trigger-active');
-	obj.find('.details').hide('slow');
-    });
+    else {
+	obj.find('.details').show();
+    }
 }
 
 function hide_questionform() {

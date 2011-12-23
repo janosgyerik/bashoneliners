652d055a5e3657e362566bdf38d5397fca4428de
Janos Gyerik <janos@axiom>
2011-12-23 18:42:57 +0100

    form error style fixes

--- media/js/bashoneliners.js
+++ media/js/bashoneliners.js
@@ -45,7 +45,9 @@ function bind_details_trigger(obj) {
 
 function hide_questionform() {
     if ($('.form-show-hide-trigger').size() > 0) {
-	$('.questionform').hide();
+	if ($('.questionform .errorlist').size() == 0) {
+	    $('.questionform').hide();
+	}
 
 	$('.form-show-hide-trigger').click(function() {
 	    $(this).toggleClass('details-trigger-active');

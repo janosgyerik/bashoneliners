6eb5fb8526b9eff19da744347b862c6fee6e95bd
Janos Gyerik <janos@axiom>
2012-01-27 19:39:24 +0100

    always show explanation and limitations, theres not much point hiding them

--- media/js/bashoneliners.js
+++ media/js/bashoneliners.js
@@ -8,46 +8,7 @@
  * Date: Sat Oct  8 06:38:28 CEST 2011
  */
 
-function bind_details_trigger(obj) {
-    if (obj.find('.oneliner').size() > 3) {
-	obj.find('.details').each(function() {
-	    var trigger = $('<div class="details-trigger"/>').click(function() {
-		$(this).toggleClass('details-trigger-active').next().toggle('slow');
-	    });
-	    var has_explanation = $(this).find('.explanation').size() > 0;
-	    var has_limitations = $(this).find('.limitations').size() > 0;
-	    if (has_explanation && has_limitations) {
-		trigger.text('Show explanation and limitations');
-	    }
-	    else if (has_explanation) {
-		trigger.text('Show explanation');
-	    }
-	    else if (has_limitations) {
-		trigger.text('Show limitations');
-	    }
-	    $(this).before(trigger);
-	});
-
-	obj.find('.expand-all').prepend('<div class="expand-all-trigger">Expand all</div>');
-	obj.find('.expand-all').prepend('<div class="collapse-all-trigger">Collapse all</div>');
-
-	obj.find('.expand-all-trigger').click(function() {;
-	    obj.find('.details-trigger').addClass('details-trigger-active');
-	    obj.find('.details').show('slow');
-	});
-
-	obj.find('.collapse-all-trigger').click(function() {;
-	    obj.find('.details-trigger').removeClass('details-trigger-active');
-	    obj.find('.details').hide('slow');
-	});
-    }
-    else {
-	obj.find('.details').show();
-    }
-}
-
 $(document).ready(function() {
-    bind_details_trigger($('div.oneliners'));
 });
 
 // eof

7928bf821d6a8ebba68ff07d840e2a585badce51
Janos Gyerik <janos@axiom>
2012-02-02 18:49:54 +0100

    added created_dt to OneLiner and WishListQuestion columns

--- main/admin.py
+++ main/admin.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 from bashoneliners.main.models import HackerProfile, OneLiner, WishListQuestion, WishListAnswer
 
 class OneLinerAdmin(admin.ModelAdmin):
-    list_display = ('user', 'is_published', 'was_tweeted', 'summary', )
+    list_display = ('user', 'is_published', 'was_tweeted', 'summary', 'created_dt',)
 
 class WishListQuestionAdmin(admin.ModelAdmin):
-    list_display = ('user', 'is_published', 'is_answered', 'summary', )
+    list_display = ('user', 'is_published', 'is_answered', 'summary', 'created_dt',)
 
 class HackerProfileAdmin(admin.ModelAdmin):
     list_display = ('get_username', 'get_email', 'get_date_joined', 'display_name', 'twitter_name',)

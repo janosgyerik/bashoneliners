0006a074269efaf17d02190fc25038e8b944c9c6
Janos Gyerik <janos@axiom>
2012-04-04 20:15:14 +0200

    added some dev db rebuilder helper scripts

---
+++ scripts/BACKTOSAFE.sh
@@ -0,0 +1,4 @@
+# _source_ this file to set the environment variable, like this:
+# . ./scripts/BACKTOSAFE.sh
+#
+export IAMDANGEROUS=
---
+++ scripts/IAMDANGEROUS.sh
@@ -0,0 +1,4 @@
+# _source_ this file to set the environment variable, like this:
+# . ./scripts/IAMDANGEROUS.sh
+#
+export IAMDANGEROUS=1

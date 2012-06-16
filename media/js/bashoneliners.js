b8117c6e933c95d3c273826c1cf92739251329da
Janos Gyerik <janos@xw8400.home>
2012-06-16 12:36:17 +0200

    fixed tabs

--- media/js/bashoneliners.js
+++ media/js/bashoneliners.js
@@ -14,69 +14,69 @@ function popup_error() {
 
 function bind_help_markdown() {
     $('.help-markdown').click(function() {
-	var url = this.href;
-	var dialog = $('<div class="loading"></div>').appendTo('body');
-	dialog.dialog({
-	    close: function(event, ui) { dialog.remove(); },
-	    height: 400,
-	    width: 500,
-	    title: 'Markdown Syntax Quick Reference'
-	});
-	dialog.load(
-	    url,
-	    '',
-	    function(responseText, textStatus, XMLHttpRequest) {
-		dialog.removeClass('loading');
-	    }
-	);
-	return false;
+        var url = this.href;
+        var dialog = $('<div class="loading"></div>').appendTo('body');
+        dialog.dialog({
+            close: function(event, ui) { dialog.remove(); },
+            height: 400,
+            width: 500,
+            title: 'Markdown Syntax Quick Reference'
+        });
+        dialog.load(
+            url,
+            '',
+            function(responseText, textStatus, XMLHttpRequest) {
+                dialog.removeClass('loading');
+            }
+            );
+        return false;
     });
 }
 
 function bind_question_answered() {
     $('.question-answered').click(function(e) {
-	e.preventDefault();
-	var question = $('.' + $(this).attr('data-parent'));
-	var remove_buttons = function() {
-	    question.find('.question-answered').remove();
-	};
-	$.ajax({
-	    url: $(this).attr('href'),
-	    success: remove_buttons,
-	    error: popup_error
-	});
+        e.preventDefault();
+        var question = $('.' + $(this).attr('data-parent'));
+        var remove_buttons = function() {
+            question.find('.question-answered').remove();
+        };
+        $.ajax({
+            url: $(this).attr('href'),
+            success: remove_buttons,
+            error: popup_error
+        });
     });
 }
 
 function bind_preview_markdown() {
     $('.preview-markdown').click(function(e) {
-	e.preventDefault();
-	var source = $('#' + $(this).attr('data-source'));
-	var preview = $('#' + $(this).attr('data-source') + '-preview');
-	var update_preview = function(html) {
-	    preview.html(html);
-	    preview.addClass('well');
-	};
-	$.ajax({
-	    url: $(this).attr('href'),
-	    type: 'post',
-	    data: { text: source.val() },
-	    success: update_preview,
-	    error: popup_error
-	});
+        e.preventDefault();
+        var source = $('#' + $(this).attr('data-source'));
+        var preview = $('#' + $(this).attr('data-source') + '-preview');
+        var update_preview = function(html) {
+            preview.html(html);
+            preview.addClass('well');
+        };
+        $.ajax({
+            url: $(this).attr('href'),
+            type: 'post',
+            data: { text: source.val() },
+            success: update_preview,
+            error: popup_error
+        });
     });
 }
 
 function bind_comments_toggle() {
     $('.comments-toggle').click(function(e) {
-	$(this).toggleClass('expanded').next().toggle(0);
+        $(this).toggleClass('expanded').next().toggle(0);
     });
 }
 
 function footer_fix() {
     if ($('body').height() < $(window).height()) {
-	$('.footer').addClass('fixed-bottom');
-	$('body').css('padding-bottom', '50px');
+        $('.footer').addClass('fixed-bottom');
+        $('body').css('padding-bottom', '50px');
     }
 }
 

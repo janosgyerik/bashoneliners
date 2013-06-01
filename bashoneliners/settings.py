6131f56fb878194a00b11e4d89a48f8397aa4d5e
Janos Gyerik <janos@xw8400>
2013-06-01 17:43:27 +0200

    embed markup template filters from django.contrib.markup, as it will be removed from django in the next version

--- bashoneliners/settings.py
+++ bashoneliners/settings.py
@@ -131,7 +131,6 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'django.contrib.admindocs',
-    'django.contrib.markup',
     'django.contrib.comments',
     'django_openid_auth',
     'oneliners',

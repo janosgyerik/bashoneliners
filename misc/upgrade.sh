beb0b25dee0d33bbc843c05514d5f90265ebb78d
Janos Gyerik <janos@xw8400>
2013-06-01 14:52:52 +0200

    added scripts in misc/ that help setting up deployment by git push to server

--- misc/upgrade.sh
+++ misc/upgrade.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -e
 
-cd $(dirname "$0")
+cd $(dirname "$0")/..
 
+unset GIT_DIR
 git pull
 
+. ~/virtualenv/bashoneliners/bin/activate
 ./manage.py collectstatic --noinput
 
 touch ../tmp/restart.txt

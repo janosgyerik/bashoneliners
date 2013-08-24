f1b7e91ec706e3c7a0f1d0e0d0db9013779678c5
Janos Gyerik <janos@xw8400>
2013-08-24 18:30:00 +0200

    bugfix: should not try to get upvote/downvote of anon users

--- oneliners/templatetags/upvote.py
+++ oneliners/templatetags/upvote.py
@@ -7,22 +7,24 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def upvoted(context, oneliner):
     user = context['user']
-    try:
-        vote = oneliner.vote_set.get(user=user)
-        if vote.value > 0:
-            return 'upvoted'
-    except Vote.DoesNotExist:
-        pass
+    if user.is_authenticated():
+        try:
+            vote = oneliner.vote_set.get(user=user)
+            if vote.value > 0:
+                return 'upvoted'
+        except Vote.DoesNotExist:
+            pass
     return ''
 
 
 @register.simple_tag(takes_context=True)
 def downvoted(context, oneliner):
     user = context['user']
-    try:
-        vote = oneliner.vote_set.get(user=user)
-        if vote.value < 0:
-            return 'downvoted'
-    except Vote.DoesNotExist:
-        pass
+    if user.is_authenticated():
+        try:
+            vote = oneliner.vote_set.get(user=user)
+            if vote.value < 0:
+                return 'downvoted'
+        except Vote.DoesNotExist:
+            pass
     return ''

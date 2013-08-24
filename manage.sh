f7d23ce6ee4d6f10858b1c14590bc7382d3f5baf
Janos Gyerik <janos@xw8400>
2013-08-24 12:41:02 +0200

    slightly cleaner scripts

--- manage.sh
+++ manage.sh
@@ -3,14 +3,15 @@
 cd $(dirname "$0")
 
 projectname=$(basename $PWD)
-virtualenv=~/virtualenv/$projectname/bin/activate
-test -f $virtualenv && . $virtualenv
-
-#env=$1; shift
+virtualenv=~/virtualenv/$projectname
+test -d $virtualenv && . $virtualenv/bin/activate || {
+    echo virtualenv does not exist: $virtualenv
+    echo Create it with: virtualenv --distribute $virtualenv
+    exit 1
+}
 
 if test $# = 0; then
     python manage.py help
 else
-    #python manage.py $* --setting=local_settings.$env
     python manage.py $*
 fi
--- misc/upgrade.sh
+++ misc/upgrade.sh
@@ -5,7 +5,6 @@ cd $(dirname "$0")/..
 unset GIT_DIR
 git pull
 
-. ~/virtualenv/bashoneliners/bin/activate
-python manage.py collectstatic --noinput
+./manage.sh collectstatic --noinput
 
 touch ../tmp/restart.txt

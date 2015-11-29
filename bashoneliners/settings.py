ce6f8c926288f84fb4aa105adb95f479beff4d2b
Janos Gyerik <janos@kronos>
2015-11-29 15:47:21 +0100

    uncomment STATIC_ROOT setting

--- bashoneliners/settings.py
+++ bashoneliners/settings.py
@@ -118,7 +118,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/

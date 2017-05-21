1dbf86919a78b3182e9d624b7e462b3510c8ba22
Janos Gyerik <janos@kronos>
2017-05-21 12:56:54 +0200

    Order users by oneliner count, descending

--- oneliners/admin.py
+++ oneliners/admin.py
@@ -1,6 +1,25 @@
+from django.contrib.auth.models import User
 from django.contrib import admin
+from django.db import models
 
 from oneliners.models import HackerProfile, OneLiner
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('oneliner_count', 'id', 'username', 'email', 'first_name', 'last_name', 'is_staff')
+
+    def get_queryset(self, request):
+        qs = super(UserAdmin, self).get_queryset(request)
+        qs = qs.annotate(models.Count('oneliner'))
+        qs = qs.order_by('-oneliner__count')
+        return qs
+
+    def oneliner_count(self, obj):
+        return obj.oneliner__count
+    oneliner_count.admin_order_field = 'oneliner_count'
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 
 class OneLinerAdmin(admin.ModelAdmin):

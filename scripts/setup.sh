2d45f5f76681a6d0e51e0a16cc2bdc31384b7221
Janos Gyerik <janos@kronos>
2017-05-29 23:04:21 +0200

    Use python -m venv to create virtualenv

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@
 cd $(dirname "$0")/..
 
 virtualenv=./virtualenv
-test -d $virtualenv || virtualenv --distribute $virtualenv
+test -d $virtualenv || python -m venv $virtualenv
 
 requirements=requirements.txt
 test -f $requirements && ./pip.sh install -r $requirements

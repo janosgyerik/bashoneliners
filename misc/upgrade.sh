c46e5bae16c37588a2cb3825cb5cffca60f556eb
Janos Gyerik <info+kronos@janosgyerik.com>
2019-04-21 19:48:13 +0200

    Add script to simplify upgrading requirements

--- misc/upgrade.sh
+++ misc/upgrade.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$0")"/..
 unset GIT_DIR
 git pull
 
-./pip.sh install -r requirements.txt
+./scripts/install-or-upgrade-requirements.sh
 ./manage.sh collectstatic --noinput
 
 touch ../tmp/restart.txt
---
+++ scripts/install-or-upgrade-requirements.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cd "$(dirname "$0")"/..
+
+./pip.sh install -r requirements.txt

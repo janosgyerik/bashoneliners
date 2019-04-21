4abb6ce67f753316595a93248a8338e382b12034
Janos Gyerik <info+kronos@janosgyerik.com>
2019-04-21 19:39:20 +0200

    Add pip install to upgrade script (used during deploy)

--- misc/upgrade.sh
+++ misc/upgrade.sh
@@ -1,10 +1,13 @@
-#!/bin/sh -e
+#!/bin/sh
 
-cd $(dirname "$0")/..
+set -euo pipefail
+
+cd "$(dirname "$0")"/..
 
 unset GIT_DIR
 git pull
 
+./pip.sh install -r requirements.txt
 ./manage.sh collectstatic --noinput
 
 touch ../tmp/restart.txt

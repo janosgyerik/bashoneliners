52c89ea76690f9f8a6aab5340cac5ef8daf7d044
Janos Gyerik <info+kraken@janosgyerik.com>
2023-09-01 07:58:28 +0200

    Drop parentheses from user.is_authenticated(), it is a property

--- oneliners/templatetags/upvote.py
+++ oneliners/templatetags/upvote.py
@@ -8,7 +8,7 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def upvoted(context, oneliner):
     user = context['user']
-    if user.is_authenticated():
+    if user.is_authenticated:
         try:
             vote = oneliner.vote_set.get(user=user)
             if vote.value > 0:
@@ -21,7 +21,7 @@ def upvoted(context, oneliner):
 @register.simple_tag(takes_context=True)
 def downvoted(context, oneliner):
     user = context['user']
-    if user.is_authenticated():
+    if user.is_authenticated:
         try:
             vote = oneliner.vote_set.get(user=user)
             if vote.value < 0:

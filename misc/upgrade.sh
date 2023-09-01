aeb00a26d53a81a2112ee0308d93782a96d4445b
Janos Gyerik <info+kraken@janosgyerik.com>
2023-09-01 21:35:32 +0200

    Make post-receive more robust

--- misc/upgrade.sh
+++ misc/upgrade.sh
@@ -5,9 +5,15 @@ set -euo pipefail
 cd "$(dirname "$0")"/..
 
 unset GIT_DIR
-git pull
 
+git fetch releases
+
+branch=$(git rev-parse --abbrev-ref HEAD)
+git reset --hard "releases/$branch"
+
+./pip.sh install --upgrade pip
 ./scripts/install-or-upgrade-requirements.sh
 ./manage.sh collectstatic --noinput
+./manage.sh migrate
 
 touch ../tmp/restart.txt

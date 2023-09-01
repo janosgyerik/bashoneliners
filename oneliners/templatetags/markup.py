b630afbef2037eb128171c5da571639d315afef6
Janos Gyerik <info+kraken@janosgyerik.com>
2023-09-01 07:53:09 +0200

    Upgrade markdown dependency

--- oneliners/templatetags/markup.py
+++ oneliners/templatetags/markup.py
@@ -12,23 +12,12 @@ markup syntaxes to HTML; currently there is support for:
 """
 
 from django import template
-from django.conf import settings
-from django.utils.encoding import force_bytes, force_text
+from django.utils.encoding import force_text
 from django.utils.safestring import mark_safe
 
+import markdown as md
+
 register = template.Library()
-
-
-@register.filter(is_safe=True)
-def textile(value):
-    try:
-        import textile
-    except ImportError:
-        if settings.DEBUG:
-            raise template.TemplateSyntaxError("Error in 'textile' filter: The Python textile library isn't installed.")
-        return force_text(value)
-    else:
-        return mark_safe(force_text(textile.textile(force_bytes(value), encoding='utf-8', output='utf-8')))
 
 
 @register.filter(is_safe=True)
@@ -49,41 +38,10 @@ def markdown(value, arg=''):
     they will be silently ignored.
 
     """
-    try:
-        import markdown
-    except ImportError:
-        if settings.DEBUG:
-            raise template.TemplateSyntaxError(
-                "Error in 'markdown' filter: The Python markdown library isn't installed.")
-        return force_text(value)
+    extensions = [e for e in arg.split(",") if e]
+    if extensions and extensions[0] == "safe":
+        extensions = extensions[1:]
+        return mark_safe(md.markdown(force_text(value), extensions=extensions))
     else:
-        markdown_vers = getattr(markdown, "version_info", 0)
-        if markdown_vers < (2, 1):
-            if settings.DEBUG:
-                raise template.TemplateSyntaxError(
-                    "Error in 'markdown' filter: Django does not support versions of the Python markdown library < 2.1.")
-            return force_text(value)
-        else:
-            extensions = [e for e in arg.split(",") if e]
-            if extensions and extensions[0] == "safe":
-                extensions = extensions[1:]
-                return mark_safe(markdown.markdown(
-                    force_text(value), extensions, safe_mode=True, enable_attributes=False))
-            else:
-                return mark_safe(markdown.markdown(
-                    force_text(value), extensions, safe_mode=False))
+        return mark_safe(md.markdown(force_text(value)))
 
-
-@register.filter(is_safe=True)
-def restructuredtext(value):
-    try:
-        from docutils.core import publish_parts
-    except ImportError:
-        if settings.DEBUG:
-            raise template.TemplateSyntaxError(
-                "Error in 'restructuredtext' filter: The Python docutils library isn't installed.")
-        return force_text(value)
-    else:
-        docutils_settings = getattr(settings, "RESTRUCTUREDTEXT_FILTER_SETTINGS", {})
-        parts = publish_parts(source=force_bytes(value), writer_name="html4css1", settings_overrides=docutils_settings)
-        return mark_safe(force_text(parts["fragment"]))

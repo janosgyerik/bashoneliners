62345007625aeb8832107f4fd6932f985c86768e
Janos Gyerik <info+kraken@janosgyerik.com>
2023-09-06 18:57:56 +0200

    Drop openid login: no relevant users

--- bashoneliners/settings.py
+++ bashoneliners/settings.py
@@ -181,7 +181,6 @@ AUTHENTICATION_BACKENDS = (
     'social_core.backends.google.GoogleOAuth2',
     'social_core.backends.google.GoogleOAuth',
     'social_core.backends.twitter.TwitterOAuth',
-    'social_core.backends.open_id.OpenIdAuth',
     'django.contrib.auth.backends.ModelBackend',
 )
 

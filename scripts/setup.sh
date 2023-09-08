fe8b965bfbaf2c7735a6f0563ee98057cffdfccb
Janos Gyerik <info+kraken@janosgyerik.com>
2023-09-08 21:19:41 +0200

    Make setup script more robust and enforce precise Python version installation

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,52 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+expected_python_version_pattern='^Python [0-9]\.[0-9]+'
 
 cd $(dirname "$0")/..
 
-virtualenv=./virtualenv
-test -d $virtualenv || python -m venv $virtualenv
+info() {
+    echo "* $*"
+}
 
-requirements=requirements.txt
-test -f $requirements && ./pip.sh install -r $requirements
+error() {
+    echo "Error: $*" >&2
+}
+
+check_python() {
+    local python
+    python=$1
+    info "Checking Python binary: $python"
+    info "Expecting it is executable and with -V it outputs a version number"
+    [ -x "$python" ] && "$python" -V | grep -Eq "$expected_python_version_pattern"
+}
+
+python=${1?Usage: $0 path/to/very/specific/version/of/python}
+info "Checking Python path pattern: $python"
+
+if ! [[ $python =~ .*/python[0-9]+\.[0-9]+$ || $python =~ .*/python@[0-9]+\.[0-9]+/.*/python[0-9] ]]; then
+    error "Python path does not match pattern: .*/python[0-9]+\.[0-9]+$"
+    error "Got: $python"
+    exit 1
+fi
+
+if ! check_python "$python"; then
+    error "Output of $python -V does not match expected pattern: $expected_python_version_pattern"
+    error "Got: $("$python" -V)"
+    exit 1
+fi
+
+virtualenv=./virtualenv
+if check_python "$virtualenv"/bin/python; then
+    info "Virtual environment '$virtualenv' exists and looks ok. Exit."
+    exit
+fi
+
+set -x
+
+"$python" -m venv "$virtualenv"
+
+./pip.sh install -r requirements.txt
+
+./pip.sh install --upgrade pip
--- virtualenv.sh
+++ virtualenv.sh
@@ -1,12 +1,14 @@
-#!/bin/sh
-
 virtualenv=$(dirname "$BASH_SOURCE")/virtualenv
-if test -d $virtualenv; then
-    . $virtualenv/bin/activate
+activate=$virtualenv/bin/activate
+
+if [ -f "$activate" ]; then
+    . "$activate"
 else
     {
-    echo Error: virtualenv does not exist: $virtualenv
-    echo Create it with: ./scripts/setup.sh
+        echo "Virtualenv looks broken, not a regular file: $activate"
+        echo "Create a clean new virtualenv with:"
+        echo "  rm -fr \"$virtualenv\""
+        echo "  ./scripts/setup.sh path/to/very/specific/version/of/python"
     } >&2
     return 1
 fi

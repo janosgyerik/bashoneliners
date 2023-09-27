87fb9367d29bc7956593e963d51cc6cc85fa563a
Janos Gyerik <info+kraken@janosgyerik.com>
2023-09-27 07:50:55 +0200

    Add categorize --all

--- oneliners/management/commands/categorize.py
+++ oneliners/management/commands/categorize.py
@@ -9,6 +9,7 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument("pk", nargs="*", type=int)
+        parser.add_argument("--all", action='store_true')
 
     def handle(self, *args, **options):
         api_key = settings.OPENAI_API_KEY
@@ -18,27 +19,28 @@ class Command(BaseCommand):
         categorizer = categorization.OpenAiCategorizationComputer(api_key)
         adapter = models.CategorizationAdapter()
 
-        pklist = options['pk']
-        for pk in pklist:
-            oneliner = models.OneLiner.get(pk)
-            if oneliner.has_categories():
-                self.stderr.writelines([f'One-liner #{pk} already has categories, skipping...'])
-                for category in oneliner.get_categories():
-                    self.stderr.writelines([str(category)])
-                continue
+        oneliners = models.OneLiner.objects.filter(onelinercategory__isnull=True)
+        if not options['all']:
+            pklist = options['pk']
+            oneliners = oneliners.filter(pk__in=pklist)
 
+        for oneliner in oneliners:
             content = (
-                f"Summary: {oneliner.summary}\n"
-                f"One-liner: {oneliner.line}\n"
-                f"Explanation: {oneliner.explanation}\n"
-                f"Limitations: {oneliner.limitations}\n"
+                f"Summary: '''{oneliner.summary}'''\n"
+                f"One-liner: '''{oneliner.line}'''\n"
+                f"Explanation: '''{oneliner.explanation}'''\n"
+                f"Limitations: '''{oneliner.limitations}'''\n"
             )
+            self.stdout.writelines([
+                "Computing categories for one-liner:",
+                content,
+            ])
 
             response = categorizer.compute(content)
-            print(response.raw_response)
-            print(response.raw_response_content)
-            for category in response.categories:
-                print(category)
+            self.stdout.writelines([str(response.raw_response)])
+            self.stdout.writelines([str(response.raw_response_content)])
+            self.stdout.writelines([str(s) for s in response.categories])
+            self.stdout.writelines([""])
 
             categories = []
             for category in response.categories:

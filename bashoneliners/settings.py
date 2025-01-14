c229f74fc193c5407a9e309051eb88ac96de79a2
Janos Gyerik <janosgyerik@dragonfly>
2025-01-14 22:09:09 +0100

    Flag guard login link

--- bashoneliners/settings.py
+++ bashoneliners/settings.py
@@ -204,6 +204,7 @@ LOGIN_REDIRECT_URL = '/oneliners/login/'
 
 FLAGS = {
     # 'DEPLOYMENT_LINKS': [{'condition': 'boolean', 'value': True}],
+    # 'LOGIN': [{'condition': 'boolean', 'value': True}],
 }
 
 #

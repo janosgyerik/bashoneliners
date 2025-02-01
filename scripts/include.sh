c82037307a34e355357b8e9d3f0eaa1aff191cc0
Janos Gyerik <janosgyerik@dragonfly>
2025-02-01 11:45:40 +0100

    Remove unused helper functions and script variables

--- scripts/include.sh
+++ scripts/include.sh
@@ -1,21 +1,9 @@
 . ./virtualenv.sh
 
-apps=(bashoneliners oneliners)
-
-langs=(fr ja hu)
-
 msg() {
     echo "* $*"
 }
 
 errmsg() {
     echo "ERROR: $*"
-}
-
-is_app_dir() {
-    local appdir=$1
-    test -f "$appdir"/__init__.py || {
-        errmsg "not an app dir: $appdir/__init__.py missing"
-        return 1
-    }
 }
